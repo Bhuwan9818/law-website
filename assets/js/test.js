1fdd6f61a95e826573a5d71a86ba2c4d720d07ee
Bhuwan9818 <bhuwansingh9818@gmail.com>
2026-09-12 18:44:37 +0530

    updated the design of contact us page

--- assets/js/test.js
+++ assets/js/test.js
@@ -10,16 +10,24 @@
 //     });
 // });
 
-document.getElementById('show-more-liaison-btn').onclick = function () {
-  document.querySelector('.more-liaison').classList.toggle('d-none');
-  this.innerHTML = this.textContent === 'More...' 
-    ? '<i class="fa fa-chevron-up" style="margin-right: 6px;"></i>Less...' 
-    : '<i class="fa fa-chevron-down" style="margin-right: 6px;"></i>More...';
-};
+const liaisonBtn = document.getElementById('show-more-liaison-btn');
+if (liaisonBtn) {
+  liaisonBtn.onclick = function () {
+    const el = document.querySelector('.more-liaison');
+    if (el) el.classList.toggle('d-none');
+    this.innerHTML = this.textContent.trim().startsWith('More') 
+      ? '<i class="fa fa-chevron-up" style="margin-right: 6px;"></i>Less...' 
+      : '<i class="fa fa-chevron-down" style="margin-right: 6px;"></i>More...';
+  };
+}
 
-document.getElementById('show-more-right-btn').onclick = function () {
-  document.querySelector('.more-right-1').classList.toggle('d-none');
-  this.innerHTML = this.textContent === 'More...' 
-    ? '<i class="fa fa-chevron-up" style="margin-right: 6px;"></i>Less...' 
-    : '<i class="fa fa-chevron-down" style="margin-right: 6px;"></i>More...';
-};
+const rightBtn = document.getElementById('show-more-right-btn');
+if (rightBtn) {
+  rightBtn.onclick = function () {
+    const el = document.querySelector('.more-right-1');
+    if (el) el.classList.toggle('d-none');
+    this.innerHTML = this.textContent.trim().startsWith('More') 
+      ? '<i class="fa fa-chevron-up" style="margin-right: 6px;"></i>Less...' 
+      : '<i class="fa fa-chevron-down" style="margin-right: 6px;"></i>More...';
+  };
+}
